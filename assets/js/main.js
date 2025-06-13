ffa274acb941bda785554a7ef20e4cd2f5ea0d5f
henry-yen <henryyen0626@gmail.com>
2025-06-13 10:36:43 +0800

    刪除Search Query

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,16 +10,16 @@ function sendSearchEvent(event) {
 
   if (typeof ac === "function") {
     // 發送預設 Search query 事件（會觸發 Segments）
-    ac('record', 'Search query', {
-      query: keyword
-    });
+    // ac('record', 'Search query', {
+    //   query: keyword
+    // });
 
     // 發送自定義事件（可用於追蹤使用者意圖、建立 trigger、dashboard 分析）
     ac('record', 'searchInput', {
       keyword: keyword
     });
 
-    console.log("✅ 已送出：Search query + searchInput", keyword);
+    console.log("✅ 已送出：searchInput", keyword);
   } else {
     console.warn("⚠️ Genesys Journey SDK 尚未載入");
   }
